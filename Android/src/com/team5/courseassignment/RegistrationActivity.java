f40ab1dd641c4cf51f6d9e71080e89571754ef56
Alena Ruprecht <ar414@sussex.ac.uk>
2013-10-21 20:13:17 +0100

    added registration.xml and simple RegistrationActivity

---
+++ Android/src/com/team5/courseassignment/RegistrationActivity.java
@@ -0,0 +1,21 @@
+package com.team5.courseassignment;
+
+import com.team5.courseassigment.R;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+/*
+ * IMPORTANT NOTICE: We are only using DEBUG certificates as signature for the Google Maps API !!!
+ */
+
+
+public class RegistrationActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        setContentView(R.layout.registration);	        
+    }   
+}
