88a04814f755157ede897fc7b863bfff9aee7fdb
Karolina Bral <baunty_lt@yahoo.com>
2013-10-22 21:41:05 +0100

    Added forgotten_password.html and Forgotten Password Screen

---
+++ Android/src/com/team5/courseassignment/ForgottenPasswordActivity.java
@@ -0,0 +1,13 @@
+package com.team5.courseassignment;
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.team5.courseassigment.R;
+
+public class ForgottenPasswordActivity extends Activity {
+	 protected void onCreate(Bundle savedInstanceState) {
+	        super.onCreate(savedInstanceState);
+	        
+	        setContentView(R.layout.forgotten_password);	        
+	    }   
+}
