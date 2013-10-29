87cc348b7d31190c501f979d5de457fd9eb58ff3
Pascal Lightning <--->
2013-10-29 17:19:00 +0000

    Utilities class, contains sha1 method for encryption

---
+++ Android/src/com/team5/courseassignment/Utilies.java
@@ -0,0 +1,56 @@
+package com.team5.courseassignment;
+
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Formatter;
+
+/**
+ * helper class that contains methods which are used in many activities
+ * @author Pascal
+ *
+ */
+public class Utilies {
+	
+	/**
+	 * encrypts a String with SHA1 algorithm, returns it as a String
+	 * @param input String that need to be encrypted
+	 * @return the encrypted String
+	 */
+	public static String encryptString(String input)
+	{
+	    String sha1 = "";
+	    
+	    try {
+	        MessageDigest crypt = MessageDigest.getInstance("SHA-1");
+	        crypt.reset();
+	        crypt.update(input.getBytes("UTF-8"));
+	        sha1 = convertByteArrayToString(crypt.digest());
+	    }
+	    
+	    catch(NoSuchAlgorithmException e) {
+	        e.printStackTrace();
+	    }
+	    
+	    catch(UnsupportedEncodingException e) {
+	        e.printStackTrace();
+	    }
+	    
+	    return sha1;
+	}
+
+	private static String convertByteArrayToString(final byte[] hash) {
+	    
+		Formatter formatter = new Formatter();
+		
+	    for (byte b : hash) {
+	        formatter.format("%02x", b);
+	    }
+	    
+	    String result = formatter.toString();
+	    formatter.close();
+	    
+	    return result;
+	}
+	
+}
