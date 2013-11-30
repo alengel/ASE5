7690305c78d1709c5d01d8302aba1f89a7b18e9f
Yermek <abdramanov@bk.ru>
2013-11-30 16:52:21 +0000

    FourSquareVenue.java class Comments Format Indentation

--- Android/src/com/team5/courseassignment/FourSquareVenue.java
+++ Android/src/com/team5/courseassignment/FourSquareVenue.java
@@ -5,6 +5,16 @@ public class FourSquareVenue {
 	public String id;
 	public int distance;
 
+	/**
+	 * Constructor method of foursquare venue. Info as follows
+	 * 
+	 * @param name
+	 *            - The name of the venue/location.
+	 * @param id
+	 *            - Given id of the venue/location.
+	 * @param distance
+	 *            - Distance to the venue/location.
+	 */
 	public FourSquareVenue(String name, String id, int distance) {
 
 		this.name = name;
@@ -12,22 +22,47 @@ public class FourSquareVenue {
 		this.distance = distance;
 	}
 
+	/**
+	 * Getter method for name of the venue/location
+	 * 
+	 * @return name
+	 */
 	public String getName() {
 		return name;
 	}
 
+	/**
+	 * Setter method for name of the venue/location.
+	 * 
+	 * @param name
+	 *            - in case if user wants to change venue/location name.
+	 */
 	public void setFirstName(String name) {
 		this.name = name;
 	}
 
+	/**
+	 * Getter method for retrieving distance to the venue/location.
+	 * 
+	 * @return Integer distance
+	 */
 	public int getDistance() {
 		return distance;
 	}
 
+	/**
+	 * Setter method for distance.
+	 * 
+	 * @param distance
+	 */
 	public void setDistance(int distance) {
 		this.distance = distance;
 	}
 
+	/**
+	 * Overriden toString method to get all foursquare venues data as a chunk of
+	 * String. Especially name and distance in meters.
+	 */
 	@Override
 	public String toString() {
 		return name + ", " + distance + " m";
