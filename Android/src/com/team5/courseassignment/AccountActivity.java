a658820d59bf992d804cd8ac9a24a3914ed8fd07
Alena Ruprecht <ar414@sussex.ac.uk>
2013-11-30 21:50:32 +0000

    removed warnings

--- Android/src/com/team5/courseassignment/AccountActivity.java
+++ Android/src/com/team5/courseassignment/AccountActivity.java
@@ -22,7 +22,6 @@ import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.provider.MediaStore.MediaColumns;
 import android.util.Base64;
 import android.view.Menu;
--- Android/src/com/team5/courseassignment/ReviewActivity.java
+++ Android/src/com/team5/courseassignment/ReviewActivity.java
@@ -21,7 +21,6 @@ import android.graphics.Matrix;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.provider.MediaStore.MediaColumns;
 import android.util.Base64;
 import android.view.Menu;
