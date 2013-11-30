ff46a8fe74d3870e903d77b708bf8bdc7d57fe78
Alena Ruprecht <ar414@sussex.ac.uk>
2013-11-30 23:02:06 +0000

    fixed broken tests

--- AndroidTestTest/src/com/team5/courseassignment/test/MapActivityTest.java
+++ AndroidTestTest/src/com/team5/courseassignment/test/MapActivityTest.java
@@ -28,44 +28,44 @@ public class MapActivityTest extends
 		setActivityInitialTouchMode(false);
 
 		mActivity = getActivity();
-		mSettingsMenu = mActivity.findViewById(com.team5.courseassignment.R.id.action_settings);
-		mProfileMenu = mActivity.findViewById(com.team5.courseassignment.R.id.action_profile);
+		mSettingsMenu = mActivity
+				.findViewById(com.team5.courseassignment.R.id.action_settings);
+		mProfileMenu = mActivity
+				.findViewById(com.team5.courseassignment.R.id.action_profile);
 	}
-	
-		
-		// Test that clicking on the "profile" menu brings up the ProfileActivity.
-		@UiThreadTest
-		public void testProfileMenu() throws Exception {
 
-			// Monitor the creation of the ProfileActivity.
-			ActivityMonitor activityMonitor = 
-											  new ActivityMonitor(ProfileActivity.class.getName(),null,true);
-			getInstrumentation().addMonitor(activityMonitor);
+	// Test that clicking on the "profile" menu brings up the ProfileActivity.
+	@UiThreadTest
+	public void testProfileMenu() throws Exception {
 
-			// Click register button
-			
-			mProfileMenu.performClick();
-			
-			activityMonitor.waitForActivityWithTimeout(10000);
+		// Monitor the creation of the ProfileActivity.
+		ActivityMonitor activityMonitor = new ActivityMonitor(
+				ProfileActivity.class.getName(), null, true);
+		getInstrumentation().addMonitor(activityMonitor);
 
-			// Check that ProfileActivity was created once.
-			assertEquals(1, activityMonitor.getHits());
-		}
-		
-	
+		// Click register button
+
+		mProfileMenu.performClick();
+
+		activityMonitor.waitForActivityWithTimeout(10000);
+
+		// Check that ProfileActivity was created once.
+		assertEquals(1, activityMonitor.getHits());
+	}
+
 	// Test that clicking on the "settings" menu brings up the SettingsActivity.
 	@UiThreadTest
 	public void testSettingsMenu() throws Exception {
 
 		// Monitor the creation of the SettingsActivity.
-		ActivityMonitor activityMonitor = new ActivityMonitor(SettingsActivity.class.getName(), null, true);
-										 
+		ActivityMonitor activityMonitor = new ActivityMonitor(
+				SettingsActivity.class.getName(), null, true);
+
 		getInstrumentation().addMonitor(activityMonitor);
 
 		// Click register button
 		mSettingsMenu.performClick();
-		
-		
+
 		activityMonitor.waitForActivityWithTimeout(10000);
 
 		// Check that SettingsActivity was created once.
