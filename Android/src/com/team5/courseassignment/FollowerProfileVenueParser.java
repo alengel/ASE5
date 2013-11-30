db94fe90ab8e2c476bca50aa4a05004a1fc5ce10
Yermek <abdramanov@bk.ru>
2013-11-30 16:17:51 +0000

    Comments + Format + Indentation for FollowerProfileVenueParser.java class

--- Android/src/com/team5/courseassignment/FollowerProfileVenueParser.java
+++ Android/src/com/team5/courseassignment/FollowerProfileVenueParser.java
@@ -7,36 +7,42 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-public class FollowerProfileVenueParser
-{
+public class FollowerProfileVenueParser {
 
-	public List<FollowerProfileVenue> parseJSON(JSONObject results)
-	{
+	/**
+	 * This class parses list of venues "details" visited by the follower as a
+	 * JSONArray
+	 * 
+	 * @param results
+	 * @return reviewer_profile_venue - array list of objects to return, which
+	 *         is location_image, location_name, rating and review. If the
+	 *         response from the server is true-this parser will parse all data
+	 *         from details: [ array ]
+	 */
+
+	public List<FollowerProfileVenue> parseJSON(JSONObject results) {
 		List<FollowerProfileVenue> reviewer_profile_venue = new ArrayList<FollowerProfileVenue>();
 
 		if (results == null)
 			return reviewer_profile_venue;
 
 		JSONArray details;
-		try
-		{
+		try {
 			details = results.getJSONArray("details");
 
-			for (int i = 0, size = details.length(); i < size; i++)
-			{
+			for (int i = 0, size = details.length(); i < size; i++) {
 				JSONObject item = details.getJSONObject(i);
 				String locationImage = item.getString("location_image");
 				String locationName = item.getString("location_name");
 				String rating = item.getString("rating");
 				String review = item.getString("review");
 
-				FollowerProfileVenue followerProfileVenue = new FollowerProfileVenue(locationImage, locationName, rating, review);
+				FollowerProfileVenue followerProfileVenue = new FollowerProfileVenue(
+						locationImage, locationName, rating, review);
 				reviewer_profile_venue.add(followerProfileVenue);
 			}
-		} 
-		catch (JSONException e)
-		{
-			// TODO Auto-generated catch block
+		} catch (JSONException e) {
+
 			e.printStackTrace();
 		}
 
